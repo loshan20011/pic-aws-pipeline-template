4f237b5c9128ea74a52b676df00447ca980fcc40
loshan20011 <loshan20011@gmail.com>
2025-01-27 17:12:03 +0530

    add nginx

--- db-population/db.sh
+++ db-population/db.sh
@@ -2,5 +2,5 @@
 
 for DB_NAME in userdb shareddb consentdb; do
     echo "Creating database: $DB_NAME"
-    psql --host="$RDS_HOST" --port="5432" --username="$MASTER_USERNAME" --dbname="$MASTER_DB" -c "CREATE DATABASE $DB_NAME;"
+    psql --host="$RDS_HOST" --port="5432" --username="$MASTER_USERNAME" --no-password --dbname="$MASTER_DB" -c "CREATE DATABASE $DB_NAME;"
 done
